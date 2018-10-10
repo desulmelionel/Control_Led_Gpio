59b16ec4d54e1e194ab813093a871678fa68c457
desulmelionel <desulmelionel@gmail.com>
2018-10-10 13:09:05 -0400

    Add files via upload

--- widget.cpp
+++ widget.cpp
@@ -13,14 +13,23 @@ Widget::Widget(QWidget *parent) :
     ui->setupUi(this);
 
     //creates a timer
-    timer = new QTimer(this);
-    //setup signal and slot
-    connect(timer, SIGNAL(timeout()),this,SLOT(sw1Reader()));
-    connect(timer, SIGNAL(timeout()),this,SLOT(sw2Reader()));
-    connect(timer, SIGNAL(timeout()),this,SLOT(sw3Reader()));
-    connect(timer, SIGNAL(timeout()),this,SLOT(sw4Reader()));
+    swTimer = new QTimer(this);
+    pbTimer = new QTimer(this);
+    //setup signal and slot for updating the switch status change
+    connect(swTimer, SIGNAL(timeout()),this,SLOT(sw1Reader()));
+    connect(swTimer, SIGNAL(timeout()),this,SLOT(sw2Reader()));
+    connect(swTimer, SIGNAL(timeout()),this,SLOT(sw3Reader()));
+    connect(swTimer, SIGNAL(timeout()),this,SLOT(sw4Reader()));
+
+    //setup signal and slot for updating the switch status change
+    connect(pbTimer, SIGNAL(timeout()),this,SLOT(pb1Reader()));
+    connect(pbTimer, SIGNAL(timeout()),this,SLOT(pb2Reader()));
+    connect(pbTimer, SIGNAL(timeout()),this,SLOT(pb3Reader()));
+    connect(pbTimer, SIGNAL(timeout()),this,SLOT(pb4Reader()));
     //msec
-    timer->start(50);
+    swTimer->start(500);
+    pbTimer->start(500);
+
 }
 
 Widget::~Widget()
@@ -29,6 +38,32 @@ Widget::~Widget()
     delete ui;
 }
 
+void Widget::on_led1RadioButton_clicked()
+{
+    ledPin = 1016;
+
+}
+
+void Widget::on_led2RadioButton_clicked()
+{
+    ledPin = 1017;
+}
+
+void Widget::on_led3RadioButton_clicked()
+{
+    ledPin = 1018;
+}
+
+void Widget::on_led4RadioButton_clicked()
+{
+    ledPin = 1019;
+}
+
+
+int Widget::getLedPin() const
+{
+    return ledPin;
+}
 
 void Widget::on_onButton_clicked()
 {
@@ -72,32 +107,7 @@ void Widget::on_offButton_clicked()
     fclose(fp);
 }
 
-void Widget::on_led1RadioButton_clicked()
-{
-    ledPin = 1016;
 
-}
-
-void Widget::on_led2RadioButton_clicked()
-{
-    ledPin = 1017;
-}
-
-void Widget::on_led3RadioButton_clicked()
-{
-    ledPin = 1018;
-}
-
-void Widget::on_led4RadioButton_clicked()
-{
-    ledPin = 1019;
-}
-
-
-int Widget::getLedPin() const
-{
-    return ledPin;
-}
 
 
 void Widget::sw1Reader()
@@ -120,7 +130,6 @@ void Widget::sw1Reader()
     {
         swGlobalValue =  QString("0");
     }
-    qDebug()<<"print value"<<swGlobalValue;
     ui->sw1Label->setText(swGlobalValue);
 }
 
@@ -144,7 +153,6 @@ void Widget::sw2Reader()
     {
         swGlobalValue =  QString("0");
     }
-    qDebug()<<"print value"<<swGlobalValue;
     ui->sw2Label->setText(swGlobalValue);
 }
 
@@ -168,7 +176,6 @@ void Widget::sw3Reader()
     {
         swGlobalValue =  QString("0");
     }
-    qDebug()<<"print value"<<swGlobalValue;
     ui->sw3Label->setText(swGlobalValue);
 }
 
@@ -192,6 +199,97 @@ void Widget::sw4Reader()
     {
         swGlobalValue =  QString("0");
     }
-    qDebug()<<"print value"<<swGlobalValue;
     ui->sw4Label->setText(swGlobalValue);
+}
+
+void Widget::pb1Reader()
+{
+    int pin = 1020;
+    char value;
+     QString swGlobalValue;
+    FILE * fp;
+    QString valueString =
+            QString("/sys/class/gpio/gpio%1/value").arg(pin);
+    fp = fopen(valueString.toLatin1(), "rb+");
+    rewind(fp);//Set pointer to begining of the file
+    fread(&value, sizeof(char),1, fp);
+    fclose(fp);
+    if(value=='1')
+    {
+        swGlobalValue = QString("1");
+    };
+    if(value=='0')
+    {
+        swGlobalValue =  QString("0");
+    }
+    ui->pb1Label->setText(swGlobalValue);
+}
+
+void Widget::pb2Reader()
+{
+    int pin = 1021;
+    char value;
+     QString swGlobalValue;
+    FILE * fp;
+    QString valueString =
+            QString("/sys/class/gpio/gpio%1/value").arg(pin);
+    fp = fopen(valueString.toLatin1(), "rb+");
+    rewind(fp);//Set pointer to begining of the file
+    fread(&value, sizeof(char),1, fp);
+    fclose(fp);
+    if(value=='1')
+    {
+        swGlobalValue = QString("1");
+    };
+    if(value=='0')
+    {
+        swGlobalValue =  QString("0");
+    }
+    ui->pb2Label->setText(swGlobalValue);
+}
+
+void Widget::pb3Reader()
+{
+    int pin = 1022;
+    char value;
+     QString swGlobalValue;
+    FILE * fp;
+    QString valueString =
+            QString("/sys/class/gpio/gpio%1/value").arg(pin);
+    fp = fopen(valueString.toLatin1(), "rb+");
+    rewind(fp);//Set pointer to begining of the file
+    fread(&value, sizeof(char),1, fp);
+    fclose(fp);
+    if(value=='1')
+    {
+        swGlobalValue = QString("1");
+    };
+    if(value=='0')
+    {
+        swGlobalValue =  QString("0");
+    }
+    ui->pb3Label->setText(swGlobalValue);
+}
+
+void Widget::pb4Reader()
+{
+    int pin = 1023;
+    char value;
+     QString swGlobalValue;
+    FILE * fp;
+    QString valueString =
+            QString("/sys/class/gpio/gpio%1/value").arg(pin);
+    fp = fopen(valueString.toLatin1(), "rb+");
+    rewind(fp);//Set pointer to begining of the file
+    fread(&value, sizeof(char),1, fp);
+    fclose(fp);
+    if(value=='1')
+    {
+        swGlobalValue = QString("1");
+    };
+    if(value=='0')
+    {
+        swGlobalValue =  QString("0");
+    }
+    ui->pb4Label->setText(swGlobalValue);
 }
--- widget.h
+++ widget.h
@@ -23,7 +23,8 @@ public:
     ~Widget();
 
     int getLedPin() const;
-    QTimer *timer;
+    QTimer *swTimer;
+    QTimer *pbTimer;
 private slots:
 
     void on_onButton_clicked();
@@ -39,11 +40,16 @@ private slots:
     void on_led4RadioButton_clicked();
 
 public slots:
+    //slots designed to read the values of the switches and display them
     void sw1Reader();
     void sw2Reader();
     void sw3Reader();
     void sw4Reader();
 
+    void pb1Reader();
+    void pb2Reader();
+    void pb3Reader();
+    void pb4Reader();
 
 private:
     Ui::Widget *ui;
